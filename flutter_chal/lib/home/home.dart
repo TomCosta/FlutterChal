2ac0c6d90f37bb4a571a62b8db2604909f6a3a2a
Everton Costa <sys.everton@gmail.com>
2020-03-09 17:34:10 -0300

    Screenshots.

--- flutter_chal/lib/home/home.dart
+++ flutter_chal/lib/home/home.dart
@@ -146,7 +146,8 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     )
     ],
-    ), // This trailing comma makes auto-formatting nicer for build methods.
+    ), 
+    drawer: Drawer(),// This trailing comma makes auto-formatting nicer for build methods.
   );
   }
 }
--- flutter_chal/lib/movie_details/movie_details.dart
+++ flutter_chal/lib/movie_details/movie_details.dart
@@ -91,6 +91,7 @@ class MovieDetails extends StatelessWidget {
           )
         ],
       ),
+      drawer: Drawer(),
     );
   }
 }
